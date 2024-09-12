9078f67b3bdc180a0b973296a655f53037db46d1
Maria Clara Fontenele Silva <129391819+CraraMaria@users.noreply.github.com>
2024-09-11 21:17:26 -0300

    Update Meusite.py

--- Meusite.py
+++ Meusite.py
@@ -99,7 +99,7 @@ with col2:
 st.divider()
 
 # URL do arquivo PDF no GitHub
-pdf_url = "https://github.com/CraraMaria/Curriculo_doc/blob/main/Maria_Clara_CV.pdf"
+pdf_url = "https://raw.githubusercontent.com/CraraMaria/Curriculo_doc/main/Maria_Clara_CV.pdf"  # Atualize com o link correto
 
 # Cria o botão de download
 st.markdown(f'<a href="{pdf_url}" download="Maria_Clara_CV.pdf">Baixar PDF</a>', unsafe_allow_html=True)
