599cbdc4b6ca744c2f855cbeba2c0d6197725f3a
Maria Clara Fontenele Silva <129391819+CraraMaria@users.noreply.github.com>
2024-10-29 08:32:10 -0300

    Update Meusite.py

--- Meusite.py
+++ Meusite.py
@@ -96,8 +96,8 @@ st.markdown(f'<a href="{pdf_url}" download="Currículo_Maria_Clara.pdf">Baixar e
 
 with tab2:
     # Conteúdo para a aba 2
-    st.write("Mais sobre mim")
-    st.image("fotodeperfil.png", caption="Maria Clara", width=200)
+    # st.write("Mais sobre mim")
+    # st.image("fotodeperfil.png", caption="Maria Clara", width=200)
 
     st.write("EM CONSTRUÇÃO 🛠👩‍🚒")
 
