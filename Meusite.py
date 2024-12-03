3b5d3ff0e535e2657ce9636e719b00568530ac45
Maria Clara Fontenele Silva <129391819+CraraMaria@users.noreply.github.com>
2024-12-02 22:19:30 -0300

    Update Meusite.py

--- Meusite.py
+++ Meusite.py
@@ -23,6 +23,12 @@ with tab1:
         
         st.divider()
         st.subheader(":blue[EXPERIÊNCIA]")
+        st.write("**BENU SISTEMAS**")
+        st.caption("Software de Gestão Completo Para Controladoras de Pragas")
+        st.caption("O BENU é um sistema ERP, projetado para otimizar a gestão de recursos e processos nas empresas")
+
+        st.divider()
+        st.subheader(":blue[PROJETOS]")
         st.write("**Projeto Currículo** — _Projeto Python_")
         st.caption("Criação deste currículo interativo usando Streamlit.")
         st.write("**Projeto Guia do Universitário** — _Projeto Integrador_")
@@ -30,11 +36,6 @@ with tab1:
         st.write("**Projeto Metamorfo** — _Projeto de Extensão_")
         st.caption("Aprimoramento da segurança digital para leigos, com foco na tranquilidade ao usar tecnologia.")
 
-        st.divider()
-        st.subheader(":blue[PROJETOS]")
-        st.write("**BENU SISTEMAS**")
-        st.caption("Software de Gestão Completo Para Controladoras de Pragas")
-        st.caption("O BENU é um sistema ERP, projetado para otimizar a gestão de recursos e processos nas empresas")
     
         
         st.divider()
